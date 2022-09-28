01c40122cdc6f383a3e951a0bfad6254104c4bb5
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-28 12:07:45 +0530

    modified : Lambda function to validate password

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -3,35 +3,49 @@ package bridgeLabz.LambdaExpression;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+//interface having method with no body
 @FunctionalInterface
 interface UserRegistration{
-	String userInput(String input);
+	String isValidUserInput(String input);
 }
 public class LambdaExpression {
 	public static void main(String[] args) {
+		//this expression implements UserResgistration based on user inputs
+		//to validate first name using lambda expression
 		UserRegistration firstNameReference =(String input) -> {
 	        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
 			Matcher matcher = pattern.matcher(input);
             System.out.println(matcher.matches());
 			return input;		
             };
-		firstNameReference.userInput("Rajashekhar");	
-		
-	UserRegistration secondNameReference =(String input) -> {
-        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
-		Matcher matcher = pattern.matcher(input);
-        System.out.println(matcher.matches());
-		return input;		
-        };
-        secondNameReference.userInput("Reddy");
-        
+		    firstNameReference.isValidUserInput("Rajashekhar");
+		    
+		//to validate second name using lambda expression
+		UserRegistration secondNameReference =(String input) -> {
+	        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+			Matcher matcher = pattern.matcher(input);
+	        System.out.println(matcher.matches());
+			return input;		
+	        };
+	        secondNameReference.isValidUserInput("Reddy");
+	        
+	    //to validate email using lambda expression
         UserRegistration eMailReference =(String input) -> {
             Pattern pattern = Pattern.compile("^[a][b][c].[x][y][z][@][b][l].[c][o].[i][n]$");
     		Matcher matcher = pattern.matcher(input);
             System.out.println(matcher.matches());
     		return input;		
             };
-            eMailReference.userInput("abc.xyz@bl.co.in");
+            eMailReference.isValidUserInput("abc.xyz@bl.co.in");
+            
+    	//to validate password using lambda expression
+        UserRegistration passwordReference =(String input) -> {
+                Pattern pattern = Pattern.compile("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[#?!@$%^&*-]).{8,}$");
+        		Matcher matcher = pattern.matcher(input);
+                System.out.println(matcher.matches());
+        		return input;		
+                };
+                passwordReference.isValidUserInput("R@ja12345");    
 	}	
 }
 
